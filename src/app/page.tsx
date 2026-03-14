5a22bd1d84942ec46725cf7c3ae44209170e6e7b
Flamespiker <147786440+Flamespiker@users.noreply.github.com>
2026-03-14 14:51:41 -0600

    feat: Change main page (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold text-gray-900">
-        🎯 The Big Habit Tracker 📅
+        🎯 The Biggest Habit Tracker 📅
       </h1>
       <p className="mt-4 text-lg text-gray-500">
         Your personal habit & goal tracking app
